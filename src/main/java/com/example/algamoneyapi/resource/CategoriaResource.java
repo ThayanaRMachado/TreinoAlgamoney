c4676250f52bbb44e35bde9a772ef52a8bdd2cc9
ThayanaRMachado <thayanarm@hotmail.com>
2020-11-05 16:22:15 -0300

    Cadatrando uma nova categoria: Pegar a uri que está aparecendo no Location do Postman e retornar uma nova categoria.

--- src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
@@ -2,6 +2,7 @@ package com.example.algamoneyapi.resource;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,10 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -40,4 +41,10 @@ public class CategoriaResource {
 		response.setHeader("Location", uri.toASCIIString());
 		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
+	
+	// busca categoria por id
+		@GetMapping ("/{codigo}")
+		public Optional<Categoria> buscarPeloCodigo (@PathVariable Long codigo) {
+			return categoriaRepository.findById(codigo);
+		}
 }
