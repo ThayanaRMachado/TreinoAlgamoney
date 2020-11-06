8abf7f6df98bd9ef04c56e7bbb48e2993d25c7d1
ThayanaRMachado <thayanarm@hotmail.com>
2020-11-06 19:41:05 -0300

    Cadatrando uma nova pessoa com POST-Salvar a primeira pessoa

--- src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
@@ -1,11 +1,21 @@
 package com.example.algamoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoneyapi.model.Pessoa;
 import com.example.algamoneyapi.repository.PessoaRepository;
@@ -21,4 +31,21 @@ public class PessoaResource {
 	public List<Pessoa> listar() {
 		return pessoaRepository.findAll();
 	}
+	
+	@PostMapping
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public void criar(@RequestBody Pessoa pessoa) {
+		pessoaRepository.save(pessoa);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
