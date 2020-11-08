072ae2578b95513f8ccd99df9ee27f39109b954b
ThayanaRMachado <thayanarm@hotmail.com>
2020-11-08 12:07:30 -0300

    Cadatrando uma nova categoria: Retornar a pessoa salva

--- src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
@@ -2,6 +2,7 @@ package com.example.algamoneyapi.resource;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +34,6 @@ public class PessoaResource {
 	}
 	
 	@PostMapping
-	@ResponseStatus(value = HttpStatus.CREATED)
 	public ResponseEntity<Pessoa> criar(@RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 		
@@ -41,6 +42,11 @@ public class PessoaResource {
 			response.setHeader("Location", uri.toASCIIString());
 			
 			return ResponseEntity.created(uri).body(pessoaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Optional<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
+		return pessoaRepository.findById(codigo);
 	}
 }
 
