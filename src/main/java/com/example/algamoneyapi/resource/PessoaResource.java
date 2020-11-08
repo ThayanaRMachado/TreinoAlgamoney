2d208d02cd33642ba05159a27cab396d410f1822
ThayanaRMachado <thayanarm@hotmail.com>
2020-11-08 14:25:18 -0300

    Usando eventos para adicionar header Location

--- src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,12 +21,16 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.algamoneyapi.event.RecursoCriadoEvent;
 import com.example.algamoneyapi.model.Pessoa;
 import com.example.algamoneyapi.repository.PessoaRepository;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaResource {
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
@@ -37,12 +43,8 @@ public class PessoaResource {
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
-		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/codigo")
-				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
-			response.setHeader("Location", uri.toASCIIString());
-			
-			return ResponseEntity.created(uri).body(pessoaSalva);
+			publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+			return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
