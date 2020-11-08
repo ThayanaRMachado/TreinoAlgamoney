d1d76fd248db8eba50827ceb292f5bcb9ba15228
ThayanaRMachado <thayanarm@hotmail.com>
2020-11-08 16:48:32 -0300

    Removendo pessoa com DELETE

--- src/main/java/com/example/algamoneyapi/event/listener/RecursoCriadoListener.java
+++ src/main/java/com/example/algamoneyapi/event/listener/RecursoCriadoListener.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.resource.HttpResource;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoneyapi.event.RecursoCriadoEvent;
@@ -18,14 +17,14 @@ public class RecursoCriadoListener implements ApplicationListener<RecursoCriadoE
 	public void onApplicationEvent(RecursoCriadoEvent recursoCriadoEvent) {
 		HttpServletResponse response = recursoCriadoEvent.getResponse();
 		Long codigo = recursoCriadoEvent.getCodigo();
-
+		
 		adicionarHeaderLocation(response, codigo);
 	}
 
 	private void adicionarHeaderLocation(HttpServletResponse response, Long codigo) {
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/codigo").buildAndExpand(codigo).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(codigo).toUri();
 		response.setHeader("Location", uri.toASCIIString());
-
 	}
 
 }
